0ca76b76989a4d2901be101a889597c3937f045e
Pratheek Hegde <ptk609@gmail.com>
2018-02-16 23:56:21 +0530

    Added breadcrumb metadata to the router

--- src/router/index.js
+++ src/router/index.js
@@ -13,21 +13,33 @@ const router = new Router({
   routes: [
     {
       path: '/',
-      component: HelloWorld
+      component: HelloWorld,
+      meta: {
+        breadCrumb: 'Hello World'
+      }
     },
     {
       path: '/earth',
       component: HelloEarth,
+      meta: {
+        breadCrumb: 'Earth'
+      },
       children: [
         {
           path: 'moon',
-          component: HelloMoon
+          component: HelloMoon,
+          meta: {
+            breadCrumb: 'Moon'
+          }
         }
       ]
     },
     {
       path: '/saturn',
       component: HelloSaturn,
+      meta: {
+        breadCrumb: 'Saturn'
+      },
       children: [
         {
           path: ':moon',
